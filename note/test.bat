9c47189a6c0c2ef4e944a42c389c9c10b3c2c7ab
bkk <qq12352100@163.com>
2020-03-12 13:03:48 +0800

    add test.bat

---
+++ note/test.bat
@@ -0,0 +1,40 @@
+::不加@时，在运行时，会在窗口显示出这条命令,而加了@, 只会显示出 echo后面你要显示出的东西
+@echo off 
+@echo --  输出文字       -- 	rem 跟在输出后面会输出
+
+:: 将c盘下所有文件名输出到a.txt中，如果没有a文件，则在bat同一目录下创建一个
+rem dir c:\*.* >a.txt 
+
+::md D:\test  					rem 在D盘下创建test文件夹
+
+set /p var=输入一个数:
+:: EQU 等于 || NEQ 不等于 || LSS 小于 || LEQ 小于或等于 ||  GTR 大于 || GEQ 大于或等于
+if %var% EQU 1 (
+	goto FIRST 
+) else (
+	goto SECOND 
+)
+
+
+cd /d %~dp0 					rem 打开当前路径
+ 
+GOTO SECOND
+:FIRST
+@echo I AM FIRST
+:SECOND
+@echo I AM SECOND
+
+set a=aa1bb1aa2bb2
+@echo %a%
+set b=12
+@echo %b%
+set /a c=39/10           
+@echo %c%
+
+@echo %CD%						rem 代表当前目录的字符串
+@echo %DATE%					rem 当前日期
+@echo %TIME%					rem 当前时间
+
+
+
+pause
