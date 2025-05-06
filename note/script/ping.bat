7c825161a0f19edafc3e671ce99b28c384a42996
bkk <qq12352100@163.com>
2025-05-06 16:52:11 +0800

    git

--- note/script/ping.bat
+++ note/script/ping.bat
@@ -1,10 +1,10 @@
 @echo off
+setlocal enabledelayedexpansion
 :: 设置变量
 set "host=10.100.255.254"  :: 目标主机IP地址
 set "logdir=D:\logs"       :: 日志目录
 set "logfile=%logdir%\ping_%host%_%date:~0,4%-%date:~5,2%-%date:~8,2%.log"  :: 日志文件名
 set "max_loop=36000"        :: 最大循环次数（防止无限运行）
-set "interval=1"           :: 每次 ping 的间隔时间（秒）
 
 :: 创建日志目录（如果不存在）
 if not exist "%logdir%" (
@@ -19,7 +19,6 @@ if exist "%logfile%" (
 :: 初始化计数器
 set /a count=0
 
-echo 1 >> "%logfile%"
 :loop
 :: 检查是否达到最大循环次数
 if %count% geq %max_loop% (
@@ -28,12 +27,28 @@ if %count% geq %max_loop% (
 )
 
 for /f "tokens=* skip=2" %%A in ('ping %host% -n 1') do (
+    set "pings=%%A"
     :: 检查 ping 输出是否为空
-    if "%%A"=="" (
+    if "!pings!"=="" (
         echo [ERROR] ping 输出为空
     ) else (
-        echo %date:~0,-3% %time:~0,-3% %%A >> %logfile%
-        echo %date:~0,-3% %time:~0,-3% %%A
+        echo %date:~0,-3% %time:~0,-3% !pings!
+        for /f "tokens=1-5 delims= " %%B in ("!pings!") do (
+            REM echo Token1: %%B & echo Token2: %%C & echo Token3: %%D & echo Token4: %%E & echo Token5: %%F
+            :: 提取数字
+            for /f "tokens=2 delims==" %%v in ("%%F") do (
+                set "time_ms=%%v"
+            )
+            :: 从"time=15ms"中进一步提取纯数字部分
+            for /f "tokens=1 delims=ms" %%n in ("%time_ms%") do (
+                if %%n geq 10 (
+                    echo %date:~0,-3% %time:~0,-3% !pings! >> %logfile%
+                    echo !pings!
+                )
+            )
+
+        )
+        REM echo %date:~0,-3% %time:~0,-3% %%A
         timeout /t 1 /nobreak > nul
         set /a count+=1
     )
@@ -42,3 +57,4 @@ for /f "tokens=* skip=2" %%A in ('ping %host% -n 1') do (
 
 :end
 echo [INFO] 脚本运行结束。
+pause
--- note/script/python/student.py
+++ note/script/python/student.py
@@ -24,11 +24,11 @@ def readExcel():
         for index, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2): # min_row=2 跳过标题行，values_only=True 返回单元格值而不是对象
             cell_time = datetime.strptime(row[0], '%Y年%m月%d日 %H:%M')
             yesterday = (datetime.now() - timedelta(days=1)).date()
-            # if cell_time.date() == datetime(2025, 4, 15).date(): # 作业时间
+            yesterday = datetime(2025, 4, 30).date()
             if cell_time.date() == yesterday: # 作业时间
                 my_set.add(row[2])
         workbook.close()
-        # os.remove(file_path)
+        os.remove(file_path)
         print("文件已删除！")
     else:
         print("文件不存在...")
