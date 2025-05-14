08a2b9d41f041a9a8e4ccb030251642c7aa61a62
bkk <qq12352100@163.com>
2025-05-14 16:39:01 +0800

    startsh

--- note/script/python/web/start_update_app.sh
+++ note/script/python/web/start_update_app.sh
@@ -1,10 +1,10 @@
-wget -O app.py https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/app.py
-wget -O TOTP.py https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/TOTP.py
-wget -O online_note.py https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/online_note.py
-wget -O stock.py https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/stock.py
-sleep 1
-systemctl restart pyweb
-sleep 2
-systemctl status pyweb
+ps -ef | grep '[a]pp.py' | awk '{print $2}' | xargs kill -9
+wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/app.py
+wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/TOTP.py
+wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/online_note.py
+wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/stock.py
+wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/start_update_app.sh
+sleep 3
+nohup python3 app.py > log.file 2>&1 &
 sleep 1
 tail -30f log.file
