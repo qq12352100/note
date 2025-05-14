a75fad9c4edc68c729cb1d2944312dce4986a8f0
bkk <qq12352100@163.com>
2025-05-14 17:18:02 +0800

    web

--- note/script/python/web/start_update_app.sh
+++ note/script/python/web/start_update_app.sh
@@ -1,10 +1,10 @@
 ps -ef | grep '[a]pp.py' | awk '{print $2}' | xargs kill -9
-wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/app.py
-wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/TOTP.py
-wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/online_note.py
-wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/stock.py
+wget -O app.py https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/app.py
+wget -O TOTP.py https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/TOTP.py
+wget -O online_note.py https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/online_note.py
+wget -O stock.py https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/stock.py
 if [ ! -f "start_update_app.sh" ]; then
-    wget https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/start_update_app.sh
+    wget -O start_update_app.sh https://raw.githubusercontent.com/qq12352100/note/master/note/script/python/web/start_update_app.sh
     chmod +x start_update_app.sh
 fi
 
@@ -12,3 +12,4 @@ sleep 3
 nohup python3 app.py > log.file 2>&1 &
 sleep 1
 tail -30f log.file
+
