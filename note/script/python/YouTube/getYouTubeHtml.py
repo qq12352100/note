33b05fae47cd41bd68aaf3329f5119880f5a615e
bkk <qq12352100@163.com>
2025-08-04 12:17:04 +0800

    youtube

--- note/script/python/YouTube/getYouTubeHtml.py
+++ note/script/python/YouTube/getYouTubeHtml.py
@@ -117,8 +117,16 @@ get2()
 # getSql('youdianyisi2020')
 # endMysql()
 
-#   D:\AAA\py\yt-dlp_x86.exe --proxy socks4://127.0.0.1:10808 -P "D:/test" https://www.youtube.com/playlist?list=PLqid5YompiAgdz7RzDELakD24DaJV8OZO
-#   yt-dlp_x86.exe --proxy socks4://127.0.0.1:10808 -P "E:/test" https://www.youtube.com/playlist?list=PLlD7SeKBB31cwPtRZgsbceAJye9k8r1ZO -f "bv+ba" 
+'''
+E:\YouTube\yt-dlp.exe --proxy socks4://127.0.0.1:10808 -P "E:/YouTube" https://www.youtube.com/playlist?list=PLqid5YompiAgdz7RzDELakD24DaJV8OZO
+
+yt-dlp.exe --proxy socks4://127.0.0.1:10808 -P "E:/YouTube" https://www.youtube.com/playlist?list=PLlD7SeKBB31cwPtRZgsbceAJye9k8r1ZO -f "bv+ba" 
+
+
+
+
+
+'''
 
 
 
